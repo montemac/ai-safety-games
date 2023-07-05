a6e304a6ae1b0c7498951f4e2aa2a378a5486e02
montemac <m.macdiarmid@gmail.com>
2023-07-05 15:46:39 -0700

    This curve is nice!  Seems to be able to learn to get high reward pretty accurately with enough training data on a 2L attention-only model...

--- scripts/decision_transformer_demo.py
+++ scripts/decision_transformer_demo.py
@@ -15,20 +15,23 @@ lt.monkey_patch()
 
 utils.enable_ipython_reload()
 
+torch.set_printoptions(sci_mode=False)
+
 # %%
 # Train!
 
 # Hyperparams
 SEED = 0
 DEVICE = "cuda:0"
-GAME_STEPS = 20
-N_GAMES = 2000
-NUM_EPOCHS = 300
+GAME_STEPS = 10
+NUM_GAMES = 8000
+NUM_EPOCHS = 600
+NUM_TEST_GAMES = 100
 
 # Initialize a simple test model
 model = models.DecisionTransformer(
     models.DecisionTransformerConfig(
-        n_layers=1,
+        n_layers=2,
         d_model=64,
         d_head=8,
         d_state=2,
@@ -50,12 +53,14 @@ model = models.DecisionTransformer(
 
 rng = np.random.default_rng(seed=SEED)
 # Initialize the training RSA tensors
-rtgs = torch.zeros((N_GAMES, GAME_STEPS), dtype=torch.float32).to(DEVICE)
-states = torch.zeros((N_GAMES, GAME_STEPS, 2), dtype=torch.float32).to(DEVICE)
-actions = torch.zeros((N_GAMES, GAME_STEPS), dtype=torch.int64).to(DEVICE)
+rtgs = torch.zeros((NUM_GAMES, GAME_STEPS), dtype=torch.float32).to(DEVICE)
+states = torch.zeros((NUM_GAMES, GAME_STEPS, 2), dtype=torch.float32).to(
+    DEVICE
+)
+actions = torch.zeros((NUM_GAMES, GAME_STEPS), dtype=torch.int64).to(DEVICE)
 
 # Run the games to populate the training tensors
-for game_idx in tqdm(range(N_GAMES)):
+for game_idx in tqdm(range(NUM_GAMES)):
     # Initialize the game
     states_this = rng.integers(0, 2, size=(GAME_STEPS, 2))
     actions_this = rng.integers(0, 2, size=GAME_STEPS)
@@ -81,31 +86,54 @@ for game_idx in tqdm(range(N_GAMES)):
 # Create a test set of trajectories where the final state varies across all
 # perms, the final RTG is 1, and the preceeding states are all dummies
 # (RTG 1, state 0, 0, action 1)
-rtgs_test = torch.ones((4, GAME_STEPS), dtype=torch.float32).to(DEVICE)
-states_test = torch.zeros((4, GAME_STEPS, 2), dtype=torch.float32).to(DEVICE)
-states_test[:, -1, :] = torch.tensor(
-    [[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32
-).to(DEVICE)
-actions_test = torch.ones((4, GAME_STEPS - 1), dtype=torch.int64).to(DEVICE)
-next_actions_correct_test = torch.tensor([0, 1, 1, 0], dtype=torch.int64).to(
-    DEVICE
-)[:, None]
+with torch.no_grad():
+    # 1D test, at final position
+    rtgs_test = torch.ones((4, GAME_STEPS), dtype=torch.float32).to(DEVICE)
+    states_test = torch.zeros((4, GAME_STEPS, 2), dtype=torch.float32).to(
+        DEVICE
+    )
+    states_test[:, -1, :] = torch.tensor(
+        [[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32
+    ).to(DEVICE)
+    actions_test = torch.ones((4, GAME_STEPS - 1), dtype=torch.int64).to(
+        DEVICE
+    )
+    next_actions_correct_test = torch.tensor(
+        [0, 1, 1, 0], dtype=torch.int64
+    ).to(DEVICE)[:, None]
+
+    # Test for correct actions at all positions
+    rtgs_all_correct = (
+        torch.arange(GAME_STEPS, 0, -1).to(DEVICE).expand([NUM_TEST_GAMES, -1])
+    )
+    actions_correct = torch.logical_xor(
+        states[:NUM_TEST_GAMES, :, 0], states[:NUM_TEST_GAMES, :, 1]
+    ).to(dtype=torch.int64)
+    states_test_games = states[:NUM_TEST_GAMES].clone()
 
 # Train the model
 optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.01)
 
 training_results = []
 for epoch in tqdm(range(NUM_EPOCHS)):
-    # Run on test set
+    # Run on test sets
     with torch.no_grad():
+        # 1D final position
         final_logits = model(
             rtgs=rtgs_test,
             states=states_test,
             actions=actions_test,
         )[:, [-1], :]
-        loss_test = model.loss_fn(
+        loss_test_1d = model.loss_fn(
             logits=final_logits, actions=next_actions_correct_test
         )
+        # All positions
+        logits = model(
+            rtgs=rtgs_all_correct,
+            states=states_test_games,
+            actions=actions_correct,
+        )
+        loss_test_games = model.loss_fn(logits=logits, actions=actions_correct)
 
     # Zero the parameter gradients
     optimizer.zero_grad()
@@ -121,14 +149,16 @@ for epoch in tqdm(range(NUM_EPOCHS)):
         {
             "epoch": epoch,
             "loss_train": loss.item(),
-            "loss_test": loss_test.item(),
+            "loss_test_1d": loss_test_1d.item(),
+            "loss_test_games": loss_test_games.item(),
         }
     )
 
 training_results = pd.DataFrame(training_results)
 px.line(
     training_results.melt(
-        id_vars=["epoch"], value_vars=["loss_train", "loss_test"]
+        id_vars=["epoch"],
+        value_vars=["loss_train", "loss_test_1d", "loss_test_games"],
     ),
     x="epoch",
     y="value",
@@ -136,26 +166,17 @@ px.line(
 ).show()
 
 # %%
-# Test the model
-
-# Create some test trajectories where the final state varies across all
-# perms, the final RTG is 1, and the preceeding states are all dummies
-# (RTG 1, state 0, 0, action 1)
-rtgs_test = torch.ones((4, GAME_STEPS), dtype=torch.float32).to(DEVICE)
-states_test = torch.zeros((4, GAME_STEPS, 2), dtype=torch.float32).to(DEVICE)
-states_test[:, -1, :] = torch.tensor(
-    [[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32
-).to(DEVICE)
-actions_test = torch.ones((4, GAME_STEPS - 1), dtype=torch.int64).to(DEVICE)
+# Test the model more thoroughly
 
 with torch.no_grad():
-    final_logits = model(
-        rtgs=rtgs_test,
-        states=states_test,
-        actions=actions_test,
-    )[:, -1, :]
-    dist = torch.distributions.categorical.Categorical(
-        logits=final_logits.squeeze()
+    logits = model(
+        rtgs=rtgs_all_correct,
+        states=states[:NUM_TEST_GAMES],
+        actions=actions_correct,
     )
+    dist = torch.distributions.categorical.Categorical(logits=logits.squeeze())
 
-dist.probs.p
+prob_correct_action = dist.probs.gather(
+    dim=-1, index=actions_correct[..., None]
+)
+px.line(prob_correct_action.mean(dim=0).cpu().numpy()).show()
