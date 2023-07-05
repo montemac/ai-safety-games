8f233dc32fbf4e30e8700c9d2a7f2c5f2741cfe3
montemac <m.macdiarmid@gmail.com>
2023-07-05 14:09:49 -0700

    This setup seems to show grokking on the action-XOR-of-state sequence problem!

--- scripts/decision_transformer_demo.py
+++ scripts/decision_transformer_demo.py
@@ -4,6 +4,7 @@
 import torch
 import torch.optim as optim
 import numpy as np
+import pandas as pd
 from tqdm.auto import tqdm
 import lovely_tensors as lt
 import plotly.express as px
@@ -21,7 +22,7 @@ DEVICE = "cuda:0"
 # Initialize a simple test model
 model = models.DecisionTransformer(
     models.DecisionTransformerConfig(
-        n_layers=8,
+        n_layers=1,
         d_model=64,
         d_head=8,
         d_state=2,
@@ -30,6 +31,7 @@ model = models.DecisionTransformer(
         device=DEVICE,
         seed=SEED,
         n_timesteps=10,
+        attn_only=True,
     )
 )
 
@@ -39,14 +41,15 @@ model = models.DecisionTransformer(
 # - Action is either 0 or 1
 # - If the action == XOR(state), then timestep reward is 1, else 0
 # - Games always last for exactly 10 timesteps
-GAME_STEPS = 1
-N_GAMES = 100
+GAME_STEPS = 10
+N_GAMES = 1000
 rng = np.random.default_rng(seed=SEED)
-# Initialize the RSA tensors
+# Initialize the training RSA tensors
 rtgs = torch.zeros((N_GAMES, GAME_STEPS), dtype=torch.float32).to(DEVICE)
 states = torch.zeros((N_GAMES, GAME_STEPS, 2), dtype=torch.float32).to(DEVICE)
 actions = torch.zeros((N_GAMES, GAME_STEPS), dtype=torch.int64).to(DEVICE)
-# Run the games
+
+# Run the games to populate the training tensors
 for game_idx in tqdm(range(N_GAMES)):
     # Initialize the game
     states_this = rng.integers(0, 2, size=(GAME_STEPS, 2))
@@ -70,38 +73,86 @@ for game_idx in tqdm(range(N_GAMES)):
     states[game_idx] = torch.tensor(states_this, dtype=torch.float32)
     actions[game_idx] = torch.tensor(actions_this, dtype=torch.int64)
 
+# Create a test set of trajectories where the final state varies across all
+# perms, the final RTG is 1, and the preceeding states are all dummies
+# (RTG 1, state 0, 0, action 1)
+rtgs_test = torch.ones((4, GAME_STEPS), dtype=torch.float32).to(DEVICE)
+states_test = torch.zeros((4, GAME_STEPS, 2), dtype=torch.float32).to(DEVICE)
+states_test[:, -1, :] = torch.tensor(
+    [[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32
+).to(DEVICE)
+actions_test = torch.ones((4, GAME_STEPS - 1), dtype=torch.int64).to(DEVICE)
+next_actions_correct_test = torch.tensor([0, 1, 1, 0], dtype=torch.int64).to(
+    DEVICE
+)[:, None]
+
 # Train the model
 NUM_EPOCHS = 200
 
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.01)
 
+training_results = []
 for epoch in tqdm(range(NUM_EPOCHS)):
-    # zero the parameter gradients
+    # Run on test set
+    with torch.no_grad():
+        final_logits = model(
+            rtgs=rtgs_test,
+            states=states_test,
+            actions=actions_test,
+        )[:, [-1], :]
+        loss_test = model.loss_fn(
+            logits=final_logits, actions=next_actions_correct_test
+        )
+
+    # Zero the parameter gradients
     optimizer.zero_grad()
 
-    # forward + backward + optimize
+    # Forward + backward + optimize
     logits = model(rtgs=rtgs, states=states, actions=actions)
     loss = model.loss_fn(logits=logits, actions=actions)
     loss.backward()
     optimizer.step()
 
     # print statistics
-    print(f"epoch {epoch + 1} loss: {loss.item():.3f}")
+    training_results.append(
+        {
+            "epoch": epoch,
+            "loss_train": loss.item(),
+            "loss_test": loss_test.item(),
+        }
+    )
+
+training_results = pd.DataFrame(training_results)
+px.line(
+    training_results.melt(
+        id_vars=["epoch"], value_vars=["loss_train", "loss_test"]
+    ),
+    x="epoch",
+    y="value",
+    color="variable",
+).show()
 
 # %%
 # Test the model
-rtgs_test = torch.ones((4, 1), dtype=torch.float32).to(DEVICE)
-states_test = torch.tensor(
+
+# Create some test trajectories where the final state varies across all
+# perms, the final RTG is 1, and the preceeding states are all dummies
+# (RTG 1, state 0, 0, action 1)
+rtgs_test = torch.ones((4, GAME_STEPS), dtype=torch.float32).to(DEVICE)
+states_test = torch.zeros((4, GAME_STEPS, 2), dtype=torch.float32).to(DEVICE)
+states_test[:, -1, :] = torch.tensor(
     [[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32
-)[:, None, :].to(DEVICE)
-actions_test = torch.zeros((4, 0), dtype=torch.int64).to(DEVICE)
+).to(DEVICE)
+actions_test = torch.ones((4, GAME_STEPS - 1), dtype=torch.int64).to(DEVICE)
 
 with torch.no_grad():
-    logits = model(
+    final_logits = model(
         rtgs=rtgs_test,
         states=states_test,
         actions=actions_test,
+    )[:, -1, :]
+    dist = torch.distributions.categorical.Categorical(
+        logits=final_logits.squeeze()
     )
-    dist = torch.distributions.categorical.Categorical(logits=logits.squeeze())
 
 dist.probs.p
