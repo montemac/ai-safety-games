91cfdefb13122ac9ed37bc4538aa09f71c97caad
montemac <m.macdiarmid@gmail.com>
2023-07-20 23:40:46 -0700

    Actually, amazingly it kinda does seem to work!  Conditioning on different goal scores produces a remarkably accurate mean performance.  However, I think having a max goal score of 0 isn't what we want, as it doesn't allow us to condition on how large a victory we want.

--- scripts/cheat_test.py
+++ scripts/cheat_test.py
@@ -21,7 +21,9 @@ from ai_safety_games import cheat, utils, models
 utils.enable_ipython_reload()
 
 # %%
-# Create some random players and a model-based player, run the game
+# Create some random players and a model-based player, run a bunch of
+# games
+
 # Load the model-based player
 MODEL_FN = "cheat_train_output/20230710T080356/model.pkl"
 with open(MODEL_FN, "rb") as file:
@@ -45,26 +47,42 @@ PROBS_TABLE = pd.DataFrame(
 )
 
 # List of players
-players = [
-    cheat.ModelCheatPlayer(model=model, goal_score=0),
-    cheat.RandomCheatPlayer(probs_table=PROBS_TABLE),
-    cheat.RandomCheatPlayer(probs_table=PROBS_TABLE),
-]
+results_list = []
+for goal_score in tqdm(range(-20, 2, 2)):
+    players = [
+        cheat.ModelCheatPlayer(model=model, goal_score=goal_score),
+        cheat.RandomCheatPlayer(probs_table=PROBS_TABLE),
+        cheat.RandomCheatPlayer(probs_table=PROBS_TABLE),
+    ]
 
-# Create the game
-game = cheat.CheatGame(
-    config=cheat.CheatConfig(num_ranks=13, num_suits=4, seed=SEED)
-)
-
-# Run the games
-scores_list = []
-for seed in tqdm(range(100)):
-    scores, winning_player, turn_cnt = cheat.run(
-        game=game,
-        players=players,
-        max_turns=model.dt_cfg.n_timesteps * game.config.num_players,
-        seed=seed,
+    # Create the game
+    game = cheat.CheatGame(
+        config=cheat.CheatConfig(num_ranks=13, num_suits=4, seed=seed)
     )
-    scores_list.append(scores)
-scores_df = pd.DataFrame(scores_list)
-print(scores_df.mean())
+
+    # Run the games
+    for seed in range(100):
+        scores, winning_player, turn_cnt = cheat.run(
+            game=game,
+            players=players,
+            max_turns=model.dt_cfg.n_timesteps * game.config.num_players,
+            seed=seed,
+        )
+        results = {
+            "goal_score": goal_score,
+            "seed": seed,
+        }
+        # Add scores as dict fields
+        for player, score in enumerate(scores):
+            results[f"score_{player}"] = score
+        results_list.append(results)
+
+results_df = pd.DataFrame(results_list)
+
+# %%
+# Plot results
+px.line(
+    results_df.groupby("goals_score").mean().reset_index(),
+    x="goals_score",
+    y="score_0",
+).show()
